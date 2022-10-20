1c466b8793218200be004f3d59fa2f2d48b7fb66
Jonny Petraglia <jpetraglia@inspirato.com>
2022-10-20 14:37:03 -0600

    Reset to actually blank repo

--- runme.sh
+++
@@ -1,5 +0,0 @@
-sfdx force:org:create --apiversion 53.0 --definitionfile config/project-scratch-def.json --durationdays 2 --setdefaultusername
-sfdx force:package:install -w 42 -p 04t3n000000lD02AAE
-sfdx force:source:deploy -x manifest.xml
-
-
